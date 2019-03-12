56871622515a3e8102284b102c72a19b50ac0b37
Mark Hsu <markhsu0973@gmail.com>
2019-03-12 13:19:54 +0800

    update cpp practice

---
+++ cpp_practice/least_squares/build/CMakeFiles/least_squares.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/least_squares.dir/src/least_squares.cpp.o"
+  "least_squares.pdb"
+  "least_squares"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/least_squares.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
