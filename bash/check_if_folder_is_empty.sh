41232a778c99fe9d087df600a20749bc95d7ef47
MarkHsu <markhsu0973@gmail.com>
2019-06-12 09:46:14 +0800

    update

--- bash/check_if_folder_is_empty.sh
+++ bash/check_if_folder_is_empty.sh
@@ -4,7 +4,7 @@ DIR="/home/mark/test_mark"
 # init
 # look for empty dir 
 if [ "$(ls -A $DIR)" ]; then
-     echo "Take action $DIR is not Empty"
+    echo "Take action $DIR is not Empty"
 else
     echo "$DIR is Empty"
 fi
