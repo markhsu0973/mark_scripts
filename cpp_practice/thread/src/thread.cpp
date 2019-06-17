804b5593c397cd240afc7397bd7a2b676210557d
MarkHsu <markhsu0973@gmail.com>
2019-06-17 17:25:33 +0800

    update thread

--- cpp_practice/thread/src/thread.cpp
+++ cpp_practice/thread/src/thread.cpp
@@ -13,13 +13,13 @@ void thread_function()
 int main()  
 {
     std::cout<<"It's thread practice code !!!"<<std::endl;
-    std::thread threadObj(thread_function);
+    std::thread thread1(thread_function);
     for (int i = 0; i < 5; i++)
     {
         std::cout<<"main function "<< i <<std::endl;
         sleep(2);
     }
-    threadObj.join();    
+    thread1.join();    
     std::cout<<"Exit of Main function"<<std::endl;
     return 0;
 }
