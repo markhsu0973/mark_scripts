fd34a68307324cdf91d206d37872d96b95188d21
MarkHsu <markhsu0973@gmail.com>
2019-06-18 11:36:33 +0800

    thread 傳值進去

--- cpp_practice/thread/src/thread.cpp
+++ cpp_practice/thread/src/thread.cpp
@@ -1,35 +1,17 @@
 #include <iostream>
 #include <thread>
-#include <unistd.h>
-void thread_function_1()
+
+using namespace std;
+
+void test_func2( int i )
 {
-    for (int i = 0; i < 5; i++)
-    {
-        std::cout<<"thread function1 "<< i <<std::endl;
-        sleep(5);
-    }
+  cout << i << endl;
 }
-void thread_function_2()
+
+int main( int argc, char** argv )
 {
-    for (int i = 0; i < 5; i++)
-    {
-        std::cout<<"thread function2 "<< i <<std::endl;
-        sleep(3);
-    }
-}
- 
-int main()  
-{
-    std::cout<<"It's thread practice code !!!"<<std::endl;
-    std::thread thread1(thread_function_1);
-    std::thread thread2(thread_function_2);
-    for (int i = 0; i < 5; i++)
-    {
-        std::cout<<"main function "<< i <<std::endl;
-        sleep(1);
-    }
-    thread1.join();
-    thread2.join();     
-    std::cout<<"Exit of Main function"<<std::endl;
-    return 0;
+  thread mThread( test_func2, 23 );
+  mThread.join();
+
+  return 0;
 }
