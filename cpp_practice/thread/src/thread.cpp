38013a75bb46bafb363377280f411969137de034
MarkHsu <markhsu0973@gmail.com>
2019-06-18 11:13:28 +0800

    執行兩個thread

--- cpp_practice/thread/src/thread.cpp
+++ cpp_practice/thread/src/thread.cpp
@@ -1,25 +1,35 @@
 #include <iostream>
 #include <thread>
 #include <unistd.h>
-void thread_function()
+void thread_function_1()
 {
     for (int i = 0; i < 5; i++)
     {
-        std::cout<<"thread function "<< i <<std::endl;
+        std::cout<<"thread function1 "<< i <<std::endl;
         sleep(5);
+    }
+}
+void thread_function_2()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        std::cout<<"thread function2 "<< i <<std::endl;
+        sleep(3);
     }
 }
  
 int main()  
 {
     std::cout<<"It's thread practice code !!!"<<std::endl;
-    std::thread thread1(thread_function);
+    std::thread thread1(thread_function_1);
+    std::thread thread2(thread_function_2);
     for (int i = 0; i < 5; i++)
     {
         std::cout<<"main function "<< i <<std::endl;
-        sleep(2);
+        sleep(1);
     }
-    thread1.join();    
+    thread1.join();
+    thread2.join();     
     std::cout<<"Exit of Main function"<<std::endl;
     return 0;
 }
