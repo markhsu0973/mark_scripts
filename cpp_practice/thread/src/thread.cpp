73f2ae9fd0822918ad5f92deb1a681d5216c7cce
MarkHsu <markhsu0973@gmail.com>
2019-06-18 09:16:25 +0800

    main for loop 印很多個字，thread for loop 印很多個字

--- cpp_practice/thread/src/thread.cpp
+++ cpp_practice/thread/src/thread.cpp
@@ -3,18 +3,22 @@
 #include <unistd.h>
 void thread_function()
 {
-
-    std::cout<<"thread function "<<std::endl;
-
+    for (int i = 0; i < 5; i++)
+    {
+        std::cout<<"thread function "<< i <<std::endl;
+        sleep(5);
+    }
 }
  
 int main()  
 {
     std::cout<<"It's thread practice code !!!"<<std::endl;
     std::thread thread1(thread_function);
-
-    std::cout<<"main function "<<std::endl;
-
+    for (int i = 0; i < 5; i++)
+    {
+        std::cout<<"main function "<< i <<std::endl;
+        sleep(2);
+    }
     thread1.join();    
     std::cout<<"Exit of Main function"<<std::endl;
     return 0;
