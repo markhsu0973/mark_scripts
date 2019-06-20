fbf162940b609299b6470cdb00ce3554ec47e08e
MarkHsu <markhsu0973@gmail.com>
2019-06-20 10:53:50 +0800

    練習thread 傳值進去

--- cpp_practice/thread/src/thread.cpp
+++ cpp_practice/thread/src/thread.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
 #include <thread>
- 
-void foo()
+#include <unistd.h>
+
+
+void timer(int n)
 {
-    std::this_thread::sleep_for(std::chrono::seconds(1));
+	std::cout<<"wait "<< n << " secs"<<std::endl;
+	for (int i=1; i < n + 1; i++)
+	{
+		std::cout<< i <<" secs"<<std::endl;
+		sleep(1);
+	}
 }
   
 int main()
 {
-    std::thread t(foo);
+    std::thread t(timer, 10);
     std::cout << "before joining,joinable=" << std::boolalpha << t.joinable() << std::endl;
     t.join();
     std::cout << "after joining, joinable=" << std::boolalpha << t.joinable() << '\n';
