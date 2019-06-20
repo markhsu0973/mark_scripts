88107b915cf5ff17c2f1a119d19c1d4b86ac9666
MarkHsu <markhsu0973@gmail.com>
2019-06-20 15:48:52 +0800

    查理教學範例mutex, condition_variable

--- cpp_practice/thread/src/thread.cpp
+++ cpp_practice/thread/src/thread.cpp
@@ -1,22 +1,67 @@
 #include <iostream>
 #include <thread>
+#include <condition_variable>
+#include <mutex>
+
 #include <unistd.h>
 
+using namespace std;
+// thread mThread2;
+std::condition_variable cv_ ;
+std::mutex mtx_ ;
+bool ready_ = false ;
 
-void timer(int n)
-{
-	std::cout<<"wait "<< n << " secs"<<std::endl;
-	for (int i=1; i < n + 1; i++)
-	{
-		std::cout<< i <<" secs"<<std::endl;
-		sleep(1);
+
+void foo(const int& n) {
+	std::cout << "Start executing foo!!\n" ;
+	// Wait until another function finishes
+	std::unique_lock<std::mutex> lock(mtx_) ;
+	while (!ready_) {
+		cv_.wait(lock) ;
+	}
+	std::cout << "I was notified and stopped waiting !!\n" ;
+	std::cout << "Set ready to its origianl state i.e. false\n" ;
+	ready_ = false ;
+	lock.unlock() ;
+	std::cout << "Unlock\n" ;
+
+	for (int i = 0 ; i < n ; i++) {
+		std::cout << "The product of 2 by " << i << " is " << 2 * i << std::endl ;
 	}
 }
-  
-int main()
+void OutputValue( int n )
 {
-    std::thread t(timer, 10);
-    std::cout << "before joining,joinable=" << std::boolalpha << t.joinable() << std::endl;
-    t.join();
-    std::cout << "after joining, joinable=" << std::boolalpha << t.joinable() << '\n';
+  
+  cout << "Number:";
+  for( int i = 0; i < n; ++ i )
+  {
+    this_thread::sleep_for( chrono::duration<int, std::milli>( 5 ) );
+    cout << " " << i;
+  }
+
+  cout << "\nfinish "<<endl;
+
+  std::lock_guard<std::mutex> lock(mtx_) ;
+  ready_ = true ;
+  std::cout << "I am freed!!\n" ;
+  cv_.notify_one() ;
+  std::cout << "Notified!!\n" ;
+}
+
+int main( int argc, char** argv )
+{
+
+  cout << "\nCall function with thread" << endl;
+  thread mThread1( OutputValue, 3 );
+  std::cout << "Call the 2nd function foo\n" ; 
+  thread mThread2(foo, 2);
+
+  if (mThread1.joinable()) {
+  	mThread1.join();	
+  }
+  if (mThread2.joinable()) {
+  	mThread2.join();	
+  }
+
+  return 0;
 }
