3e04fb1dab4feaaeb159d79cf66eac311c897ed8
MarkHsu <markhsu0973@gmail.com>
2019-06-20 17:15:28 +0800

    練用std::ref()

--- cpp_practice/thread/src/thread.cpp
+++ cpp_practice/thread/src/thread.cpp
@@ -1,67 +1,45 @@
 #include <iostream>
 #include <thread>
-#include <condition_variable>
-#include <mutex>
-
-#include <unistd.h>
 
 using namespace std;
-// thread mThread2;
-std::condition_variable cv_ ;
-std::mutex mtx_ ;
-bool ready_ = false ;
 
-
-void foo(const int& n) {
-	std::cout << "Start executing foo!!\n" ;
-	// Wait until another function finishes
-	std::unique_lock<std::mutex> lock(mtx_) ;
-	while (!ready_) {
-		cv_.wait(lock) ;
-	}
-	std::cout << "I was notified and stopped waiting !!\n" ;
-	std::cout << "Set ready to its origianl state i.e. false\n" ;
-	ready_ = false ;
-	lock.unlock() ;
-	std::cout << "Unlock\n" ;
-
-	for (int i = 0 ; i < n ; i++) {
-		std::cout << "The product of 2 by " << i << " is " << 2 * i << std::endl ;
-	}
-}
-void OutputValue( int n )
+class funcObj
 {
-  
-  cout << "Number:";
-  for( int i = 0; i < n; ++ i )
+public:
+  int iData;
+  // std::cout << "class: declare iData\n";
+  funcObj()
   {
-    this_thread::sleep_for( chrono::duration<int, std::milli>( 5 ) );
-    cout << " " << i;
+    iData = 0;
+    std::cout << "class: init iData\n";
   }
 
-  cout << "\nfinish "<<endl;
-
-  std::lock_guard<std::mutex> lock(mtx_) ;
-  ready_ = true ;
-  std::cout << "I am freed!!\n" ;
-  cv_.notify_one() ;
-  std::cout << "Notified!!\n" ;
-}
+  void operator()()
+  {
+  	std::cout << "class: iData is"<<iData<<"\n";
+    ++iData;
+    std::cout << "class: iData add 1\n";
+    std::cout << "class: iData is"<<iData<<"\n";
+  }
+};
 
 int main( int argc, char** argv )
 {
+  funcObj co;
 
-  cout << "\nCall function with thread" << endl;
-  thread mThread1( OutputValue, 3 );
-  std::cout << "Call the 2nd function foo\n" ; 
-  thread mThread2(foo, 2);
+  // copy
+  std::cout << "start thread 1\n";
+  thread mThread1( co );
+  std::cout << "thread 1 using copy\n";
+  mThread1.join();
+  cout << co.iData << endl;
 
-  if (mThread1.joinable()) {
-  	mThread1.join();	
-  }
-  if (mThread2.joinable()) {
-  	mThread2.join();	
-  }
+  // reference
+  std::cout << "start thread 1\n";
+  thread mThread2( ref( co ) );
+  std::cout << "thread 2 using copy\n";
+  mThread2.join();
+  cout << co.iData << endl;
 
   return 0;
 }
