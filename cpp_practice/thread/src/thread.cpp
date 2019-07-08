a3fc2874121618147755cdb53d817b2de49dc37f
MarkHsu <markhsu0973@gmail.com>
2019-07-08 15:26:19 +0800

    兩個thread輪流印log，有使用lock和condition variable

--- cpp_practice/thread/src/thread.cpp
+++ cpp_practice/thread/src/thread.cpp
@@ -7,54 +7,53 @@
 
 using namespace std;
 
+std::condition_variable cv1_ ;
+std::condition_variable cv2_ ;
+std::mutex mtx1_ ;
+std::mutex mtx2_ ;
 bool ready1_ = false ;
 bool ready2_ = false ;
 
 
 void foo(const int& n) {
-
     for (int i = 0; i < n; i++)
     {
-
+        std::unique_lock<std::mutex> lock1(mtx1_) ;
         while (!ready1_) {
-
+        	cv1_.wait(lock1) ;
         }
         std::cout << "foo function: ";
         std::cout << "The product of 2 by " << i << " is " << 2 * i << std::endl ;
         ready2_ = true ;
         ready1_ = false;
+        lock1.unlock() ;
+        cv2_.notify_one() ;
     }
-
-    //
 }
 void OutputValue( int n )
 {
-
     for (int i = 0; i < n; i++)
     {
-
-        cout << "OutputValue: " << i << "\n";
-
+        std::cout<<"OutputValue fuc i = "<<i<<"\n";
         ready1_ = true ;
-
-
+        cv1_.notify_one() ;
+        std::unique_lock<std::mutex> lock2(mtx1_) ;
         while (!ready2_) {
-
+        	cv2_.wait(lock2) ;
         }
+        lock2.unlock() ;
         ready2_ = false ;
 
     }
-  //
-
 }
 
 int main( int argc, char** argv )
 {
 
   cout << "\nCall function with thread" << endl;
-  thread mThread1( OutputValue, 3 );
+  thread mThread1( OutputValue, 10 );
   std::cout << "Call the 2nd function foo\n" ; 
-  thread mThread2(foo, 3);
+  thread mThread2(foo, 10);
 
   if (mThread1.joinable()) {
       mThread1.join();    
