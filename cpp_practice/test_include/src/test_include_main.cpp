5f1a6c5af806b441b7d0fc62d16748f6b631f8d1
MarkHsu <markhsu0973@gmail.com>
2019-09-17 14:10:53 +0800

    add test include

---
+++ cpp_practice/test_include/src/test_include_main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+
+int main( int argc, char** argv )
+{
+	std::cout << "test_include_main\n";
+
+  return 0;
+}
---
+++ cpp_practice/test_include/src/test_include_sub.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+
+int main( int argc, char** argv )
+{
+	std::cout << "test_include_sub\n";
+
+  return 0;
+}
