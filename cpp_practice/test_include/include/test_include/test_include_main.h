94ba0b2989e87f690bb24360ac8def6bb60933f8
MarkHsu <markhsu0973@gmail.com>
2019-09-17 15:28:08 +0800

    update

--- cpp_practice/test_include/include/test_include/test_include_main.h
+++ cpp_practice/test_include/include/test_include/test_include_main.h
@@ -1,8 +1,15 @@
 #ifndef _TEST_INCLUDE_MAIN_H_
 #define _TEST_INCLUDE_MAIN_H_
 #include <iostream>
-namespace TestIncludeMain {
+namespace IncludeMain {
 	
+	class TestIncludeMain{
+		public:
+		  void show();
+		  double add(double x, double y);
+		  
+		private:
+	}; /* End of class */
 } /* End of namespace */
 
 #endif
--- cpp_practice/test_include/src/test_include_main.cpp
+++ cpp_practice/test_include/src/test_include_main.cpp
@@ -1,8 +1,22 @@
 #include </home/mark/mark_scripts/cpp_practice/test_include/include/test_include/test_include_main.h>
 
+namespace IncludeMain {
+
+	void TestIncludeMain::show() {
+		std::cout<<"hello\n";
+	}
+	double TestIncludeMain::add(double x, double y) {
+		double z = x + y;
+		std::cout << x << " + " << y << " = " << z << "\n";
+	}
+
+} /* End of namespace */
+
 int main( int argc, char** argv )
 {
 	std::cout << "test include main \n";
-
+	IncludeMain::TestIncludeMain A;
+	A.show();
+	A.add(1.0, 5.0);
   return 0;
 }
