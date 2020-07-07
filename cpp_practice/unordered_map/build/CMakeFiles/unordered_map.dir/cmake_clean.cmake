fbc82f638f1e65240e460001cd0be4f0072e7c4f
MarkHsu <markhsu0973@gmail.com>
2020-07-07 15:20:35 +0800

    update

---
+++ cpp_practice/unordered_map/build/CMakeFiles/unordered_map.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/unordered_map.dir/src/unordered_map.cpp.o"
+  "unordered_map.pdb"
+  "unordered_map"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/unordered_map.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
