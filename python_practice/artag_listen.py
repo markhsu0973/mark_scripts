7a09637e2f16fd6713a63a942b3374a96b53eeed
MarkHsu <mark_yh_hsu@wistron.com>
2022-07-06 08:45:26 +0800

    update

--- python_practice/artag_listen.py
+++ python_practice/artag_listen.py
@@ -25,6 +25,9 @@ class ARFollower():
         self.sample_cnt = 0
         self.sample_size = 20
         self.tag_orientation_list = [0] * self.sample_size
+        self.tag_1st_x = 0
+        self.tag_1st_z = 0
+        self.tag_1st_detected = False
         while (not self.stop_tracking):
             r.sleep()
     # end of __init__(self)
@@ -32,13 +35,38 @@ class ARFollower():
     def set_cmd_vel(self, msg):
 
         try:
+            tag_id_1st_x_list = []
+            tag_id_1st_z_list = []
             for ar_tag in msg.markers:
                 detect_correct_id = False
+                self.tag_1st_detected = False
                 marker = ar_tag
                 tag_1st_x = marker.pose.pose.position.x 
                 tag_1st_z = marker.pose.pose.position.z
                 tag_dis = math.sqrt(tag_1st_x * tag_1st_x + tag_1st_z * tag_1st_z)
-                print "tag_id = ", ar_tag.id, ", tag dis = ", tag_dis
+                # print "tag_id = ", ar_tag.id, ", tag dis = ", tag_dis
+                if ar_tag.id == int(98):
+                    # print "update 1st"
+                    self.tag_1st_detected = True
+                    self.tag_1st_x = marker.pose.pose.position.x
+                    self.tag_1st_z = marker.pose.pose.position.z
+                    tag_id_1st_x_list.append(self.tag_1st_x)
+                    tag_id_1st_z_list.append(self.tag_1st_z)
+            # print len(tag_id_1st_x_list)
+
+            if len(tag_id_1st_z_list) > 1:
+                min_dis = 999
+                for i in range(len(tag_id_1st_z_list)):
+                    tag_d_x = tag_id_1st_x_list[i]
+                    tag_d_z = tag_id_1st_z_list[i]
+                    tag_dis = math.sqrt(tag_d_x * tag_d_x + tag_d_z * tag_d_z)
+                    if tag_dis < min_dis:
+                        min_dis = tag_dis
+                        self.tag_1st_x = tag_d_x
+                        self.tag_1st_z = tag_d_z
+                        # print self.tag_1st_x
+            if self.tag_1st_detected:
+                print self.tag_1st_x
         except:
             return
 
